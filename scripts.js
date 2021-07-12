7956601d0d084b4202e1378e9d4f44288965cb64
Korrina Shaw <shawk0619@gmail.com>
2021-07-12 11:56:09 -0500

    dogAPI

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const baseURL = 'https://efa-cors-anywhere.herokuapp.com/https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1';
+// console.log('Hello');
+
+let para = document.querySelector('.p');
+
+function catFacts() {
+    console.log("hello");
+    fetch(baseURL)
+        .then(response => response.json())
+        .then(json => facts(json));
+}
+
+function facts(json) {
+    while (para.firstChild) {
+        para.removeChild(para.firstChild);
+      }
+    console.log(json[0].fact);
+    let paragraph = document.createElement('p');
+    paragraph.innerText = json[0].fact;
+    para.appendChild(paragraph);
+}
